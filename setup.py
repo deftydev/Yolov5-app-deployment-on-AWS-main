73940475f9b1ef9d7c8825c936708d0eabce953a
deftydev <devanshgupta79212346@gmail.com>
2023-10-23 20:09:38 +0530

    updates

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'signLanguages',
     version= '0.0.0',
-    author= 'iNeuron',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'deftydev',
+    author_email= 'devanshgupta79212346@gmail.com',
     packages= find_packages(),
     install_requires = []
 
